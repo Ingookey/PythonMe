b15bd082495b0da208823f4b399fced449886688
alex hj <ingookey@163.com>
2020-07-12 22:13:25 +0800

    ss5

--- script/inlet.py
+++ script/inlet.py
@@ -1,10 +1,17 @@
 
 import os
-from urllib import request
+import
 
 def requestHttp(httpWeb):
-    response = request.urlopen("http://" + httpWeb, timeout=5).read().decode("utf-8")
-    print("response: {}".format(response))
+    import requests
+    import json
+    url = 'http://' + httpWeb.strip()
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+    }
+    response = requests.get(url, headers=headers)
+    code_dic = json.loads(response.content.decode("utf-8"))
+    print("code_dic: {}".format(code_dic))
     pass
 
 if __name__ == "__main__":
