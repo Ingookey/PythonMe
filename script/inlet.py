adf0314c82564cb2a93160cb56efffb5b72e33d3
alex hj <ingookey@163.com>
2020-07-12 13:19:24 +0800

    ss4

--- script/inlet.py
+++ script/inlet.py
@@ -1,10 +1,14 @@
 
 import os
-import struct
-import logging
+from urllib import request
 
-
+def requestHttp(httpWeb):
+    response = request.urlopen("http://" + httpWeb, timeout=5).read().decode("utf-8")
+    print("response: {}".format(response))
+    pass
 
 if __name__ == "__main__":
     print("env: {}".format(os.environ))
+
+    requestHttp("httpbin.org/")
     pass
