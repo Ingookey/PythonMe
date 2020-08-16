2b5234367abb94ad2b264f765b2bd8ff1076b691
alex hj <ingookey@163.com>
2020-08-16 21:05:35 +0800

    21.05

--- scriptsh/demo.sh
+++ scriptsh/demo.sh
@@ -3,6 +3,7 @@
 
 set -e
 
+
 function check_input() {
     echo "\$\*: $*"
     echo "PWD: $PWD, 0: $0"
@@ -25,10 +26,39 @@ function check_tr() {
     done
 }
 
+
+function check_awk() {
+    echo "1st 2st 3st 4st"  > ss.log
+    echo "1ss 2ss 3ss 4ss" >> ss.log
+    #awk '{print $0 $2}' ss.log
+
+    # way.1
+    while read line;
+    do
+        echo "read info from file: ${line}"
+        echo ${line} | tac -s " "
+    done < ss.log
+    # way.2
+    #cat ss.log | ( while read line; do echo $line; done )
+
+    word="show me"
+    for((i=0; i<${#word}; i++))
+    do 
+        echo ${word:i:1}
+    done
+}
+
+
+function check_grep() {
+    grep -e "re1" -e "re2" file
+    grep "main()" . -r --include *.{c,cpp} --exclude "README" --exclude-dir "ex-dir"
+}
+
+
 function main() {
     echo "main inlet"
     if [[ 1 ]]; then
-      check_tr
+      check_awk
     else
       check_input $@
     fi
